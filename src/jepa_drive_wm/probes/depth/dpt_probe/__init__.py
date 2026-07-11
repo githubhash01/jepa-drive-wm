ce2cdee306966730fc757676ab5220bcac789854
Hashim Al-Obaidi <84986222+githubhash01@users.noreply.github.com>
2026-07-11 16:15:30 +0100

    Update

--- src/jepa_drive_wm/probes/depth/dpt_probe/__init__.py
+++
@@ -1,6 +0,0 @@
-"""4-layer DPT depth probe: dense depth by fusing intermediate V-JEPA 2.1 layers ([2,5,8,11])."""
-from .config import DPTProbeConfig
-from .dataset import ImageDepthDataset
-from .head import DPTDepthHead, DptDepthProbe
-
-__all__ = ["DPTProbeConfig", "ImageDepthDataset", "DptDepthProbe", "DPTDepthHead"]
